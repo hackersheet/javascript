7f8616514f05ee37501bf82c7293725c6af9a8e1
naopoyo <5816375+naopoyo@users.noreply.github.com>
2024-07-28 00:02:22 +0900

    Update sandbox

---
+++ sandbox/next/src/components/theme-switcher.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { ComputerIcon, MoonIcon, SunIcon } from 'lucide-react';
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
+
+import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group';
+
+export default function ThemeSwitcher() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted || theme === undefined) {
+    return;
+  }
+
+  return (
+    <ToggleGroup
+      className="rounded-full border p-1"
+      size="sm"
+      type="single"
+      value={theme}
+      onValueChange={(value) => setTheme(value)}
+    >
+      <ToggleGroupItem className="rounded-full" value="dark" aria-label="Toggle dark">
+        <MoonIcon size={16} />
+      </ToggleGroupItem>
+      <ToggleGroupItem className="rounded-full" value="system" aria-label="Toggle system">
+        <ComputerIcon size={16} />
+      </ToggleGroupItem>
+      <ToggleGroupItem className="rounded-full" value="light" aria-label="Toggle light">
+        <SunIcon size={16} />
+      </ToggleGroupItem>
+    </ToggleGroup>
+  );
+}
--- sandbox/next/tailwind.config.ts
+++ sandbox/next/tailwind.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'tailwindcss';
 const config = {
   darkMode: ['class'],
   content: ['./src/**/*.{ts,tsx}'],
+  safelist: ['dark'],
   prefix: '',
   theme: {
     container: {
