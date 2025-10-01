ab381408edc3657cee7764dd5c5e114b2aaee46d
naopoyo <5816375+naopoyo@users.noreply.github.com>
2025-10-01 20:32:48 +0900

    Update index.ts

--- packages/next-document-content-components/src/index.ts
+++ packages/next-document-content-components/src/index.ts
@@ -1,1 +1,12 @@
-export * from './components';
+import CodeBlock from './components/code-block/code-block';
+import DirectoryTree from './components/code-block/directory-tree';
+import Gist from './components/gist/gist';
+import Heading from './components/heading/heading';
+import Image from './components/image/image';
+import Link from './components/link/link';
+import LinkCard from './components/link-card/link-card';
+import Mermaid from './components/mermaid/mermaid';
+import XPost from './components/x-post/x-post';
+import Youtube from './components/youtube/youtube';
+
+export { CodeBlock, DirectoryTree, Gist, Heading, Link, LinkCard, Mermaid, Image, XPost, Youtube };
